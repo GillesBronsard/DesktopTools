9f2fef629ae442b94cdc569412a233182fbbd96f
GillesHome <gilles@bronsard.com>
2019-11-09 18:34:05 +0100

    Remplacement de .pack par .grid pour le placement des frames

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def camelValid():
 #                        PROGRAMME
 #-------------------------------------------------------
 
-# creer une premiere fenetre 
+# creation fenetre principale
 window = Tk()
 
 # personnalisation de la fenetre
@@ -59,24 +59,7 @@ window.iconbitmap("includes/LogoB.ico")
 window.config(background='#fa4616')
 window.resizable(width=False, height=False) #Interdit le redimensionnement de la fenêtre
 
-# creer une frame
-frame = Frame(window, bg='#fa4616', bd=0, relief=SUNKEN)
-
-# ajouter un texte
-label_subtitle = Label(frame, text="Camelsificateur", font=("Courrier", 25), bg='#fa4616', fg='white')
-label_subtitle.pack()
-
-#ajouter un champ
-entry_name = Entry(frame, width=27)
-entry_name.pack()
-
-# ajouter la frame
-frame.pack(expand=YES)
-
-#ajouter un bouton de validation
-btn = Button(window, text="Envoyer", command=camelValid)
-btn.pack()
-
+#________________________________________________________
 # creation barre de menu
 menu_bar = Menu(window)
 
@@ -89,6 +72,46 @@ menu_bar.add_cascade(label="Fichier", menu=file_menu)
 
 # configurer la fenetre pour ajouter la barre de menu
 window.config(menu=menu_bar)
+
+#________________________________________________________
+# creation frame1
+frame1 = Frame(window, bg='#b6b7b8', bd=0, relief=SUNKEN)
+ 
+# ajouter un texte à frame1
+label_subtitle = Label(frame1, text="Frame1", font=("Courrier", 25), bg='#b6b7b8', fg='white')
+label_subtitle.grid(row = 0, column=0, sticky =S)
+ 
+#ajouter un champ à frame1
+entry_name = Entry(frame1, width=27)
+entry_name.grid(row = 1, column=0, sticky =S)
+ 
+#ajouter un bouton de validation à frame1
+btn = Button(frame1, text="Envoyer", command=camelValid)
+btn.grid(row =2, column=0, sticky=S)
+ 
+# ajouter frame1
+frame1.grid(row =0, column=0, pady=20, padx=0, ipady=0, ipadx=0)
+
+#________________________________________________________
+# creation frame2
+frame2 = Frame(window, bg='#b6b7b8', bd=0, relief=GROOVE)
+
+# ajouter un texte à frame2
+label_subtitle = Label(frame2, text="Frame2", font=("Courrier", 25), bg='#b6b7b8', fg='white')
+label_subtitle.grid(row =0, column=0, sticky =S)
+
+#ajouter un champ à frame2
+entry_name2 = Entry(frame2, width=27)
+entry_name2.grid(row =1, column=0, sticky=E)
+
+#ajouter un bouton de validation à frame2
+btn = Button(frame2, text="Envoyer", command=camelValid)
+btn.grid(row =2, sticky=S)
+
+# ajouter frame2
+frame2.grid(row =1, column=0, pady=10, padx=20, ipady=0, ipadx=0)
+
+#________________________________________________________
 
 # afficher
 window.mainloop()
